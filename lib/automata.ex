b2f456050c45ab5147c7c1484bd43c61e4633008
Farid GVM <faridgvm1@gmail.com>
2026-04-20 22:51:32 -0600

    Another Sample NFA automata

--- lib/automata.ex
+++ lib/automata.ex
@@ -57,9 +57,40 @@ defmodule Automata do
     {Automata.nfa2_q, Automata.nfa2_alpha, Automata.nfa2_delta, Automata.nfa2_q0, Automata.nfa2_accepted}
   end
 
-  def section_split(list, n, r) do
-    {left, right} = Enum.split(list, n)
-    right |> sections(r) |> Enum.map(fn x -> [left | x] |> List.flatten() end)
+  def nfa3_delta do
+    %{
+      0 => %{epsilon: MapSet.new([1, 7])},
+      1 => %{epsilon: MapSet.new([2, 3])},
+      2 => %{a: MapSet.new([4])},
+      3 => %{b: MapSet.new([5])},
+      4 => %{epsilon: MapSet.new([6])},
+      5 => %{epsilon: MapSet.new([6])},
+      6 => %{epsilon: MapSet.new([7])},
+      7 => %{a: MapSet.new([8])},
+      8 => %{b: MapSet.new([9])},
+      9 => %{b: MapSet.new([10])},
+      10 => %{},
+    }
+  end
+
+  def nfa3_q do
+    MapSet.new(0..10)
+  end
+
+  def nfa3_alpha do
+    MapSet.new([:a, :b])
+  end
+
+  def nfa3_q0 do
+    MapSet.new([0])
+  end
+
+  def nfa3_accepted do
+    MapSet.new([10])
+  end
+
+  def nfa3 do
+    {Automata.nfa3_q, Automata.nfa3_alpha, Automata.nfa3_delta, Automata.nfa3_q0, Automata.nfa3_accepted}
   end
 
   def sections(list, 1) do
@@ -67,6 +98,11 @@ defmodule Automata do
     red
   end
   def sections(list, r), do: sections(list, 1) |> Enum.filter(fn x -> length(x) >= r end)
+
+  def section_split(list, n, r) do
+    {left, right} = Enum.split(list, n)
+    right |> sections(r) |> Enum.map(fn x -> [left | x] |> List.flatten() end)
+  end
 
   def my_join({inst, []}), do: inst
   def my_join({inst, list}) do
@@ -82,11 +118,6 @@ defmodule Automata do
     [l | r]
   end
 
-  def combinations(_set, 0), do: MapSet.new([MapSet.new([])])
-  def combinations(set, 1), do: Enum.map(set, fn x -> MapSet.new([x]) end) |> MapSet.new()
-  def combinations(set, 2), do: set |> sections(2) |> join_px_secs(2) |> lists_to_maps() # This case is not needed, already covered by sections_splits(secs, 0, _m) pattern
-  def combinations(set, r), do: set |> sections(r) |> sections_splits(r - 2, 1) |> join_px_secs(r) |> lists_to_maps()
-
   def sections_splits(secs, n, m \\ 1)
   def sections_splits(secs, 0, _m), do: secs
   def sections_splits(secs, n, m) do
@@ -98,6 +129,11 @@ defmodule Automata do
   end
 
   def lists_to_maps(list), do: Enum.map(list, fn x -> MapSet.new(x) end) |> MapSet.new()
+
+  def combinations(_set, 0), do: MapSet.new([MapSet.new([])])
+  def combinations(set, 1), do: Enum.map(set, fn x -> MapSet.new([x]) end) |> MapSet.new()
+  def combinations(set, 2), do: set |> sections(2) |> join_px_secs(2) |> lists_to_maps() # This case is not needed, already covered by sections_splits(secs, 0, _m) pattern
+  def combinations(set, r), do: set |> sections(r) |> sections_splits(r - 2, 1) |> join_px_secs(r) |> lists_to_maps()
 
   def power_set(set) do
     set
@@ -125,7 +161,6 @@ defmodule Automata do
   def e_closure(_delta, []), do: MapSet.new
   def e_closure(delta, states) do
     e_closure_helper(delta, states, states)
-    # prime_transition(delta, states, :epsilon) |> MapSet.union(states)
   end
 
   def e_closure_helper(_delta, %{map: m, __struct__: _s}, acc) when m == %{}, do: acc
@@ -139,6 +174,16 @@ defmodule Automata do
     {rr, t}
   end
 
+  def prime_accept(powerset, accepted_states) do
+    powerset
+    |> Enum.filter(fn rr -> !MapSet.disjoint?(rr, accepted_states) end) |> MapSet.new()
+  end
+
+  def e_build_dfa_delta(powerset, alphabet, nfa_delta) do
+    powerset
+    |> Enum.map(fn s -> {s, Enum.map(alphabet, fn x -> t = e_closure(nfa_delta, prime_transition(nfa_delta, s, x)); {x, t |> bdd_helper} end) |> Map.new} end) |> Map.new
+  end
+
   def e_build_dfa_delta2(_delta, _alphabet, new_delta, [], _visited), do: new_delta
   def e_build_dfa_delta2(delta, alphabet, new_delta, [node | rest], visited) do
     if MapSet.member?(visited, node) do
@@ -149,35 +194,6 @@ defmodule Automata do
       neighbors = dv |> Map.values() |> Enum.map(fn n -> n |> MapSet.to_list |> hd end)
       e_build_dfa_delta2(delta, alphabet, nd, neighbors ++ rest, MapSet.put(visited, node))
     end
-  end
-
-  def e_determinize2({_q, al, d, q0, f}) do
-
-    al_prime = MapSet.delete(al, :epsilon)
-    q0_prime = MapSet.new([e_closure(d, q0)])
-    d_prime = e_build_dfa_delta2(d, al_prime, %{}, q0_prime |> MapSet.to_list, MapSet.new())
-    q_prime = d_prime |> Map.keys() |> MapSet.new
-    f_prime = q_prime |> prime_accept(f)
-
-    # q_prime = q |> power_set
-    # al_prime = MapSet.delete(al, :epsilon)
-    # d_prime = q_prime |> e_build_dfa_delta(al_prime, d)
-    # q0_prime = MapSet.new([e_closure(d, q0)])
-    # f_prime = q_prime |> prime_accept(f)
-
-    # {q_prime, al_prime, d_prime, q0_prime, f_prime} |> prune
-    {q_prime, al_prime, d_prime, q0_prime, f_prime}
-  end
-
-
-  def prime_accept(powerset, accepted_states) do
-    powerset
-    |> Enum.filter(fn rr -> !MapSet.disjoint?(rr, accepted_states) end) |> MapSet.new()
-  end
-
-  def e_build_dfa_delta(powerset, alphabet, nfa_delta) do
-    powerset
-    |> Enum.map(fn s -> {s, Enum.map(alphabet, fn x -> t = e_closure(nfa_delta, prime_transition(nfa_delta, s, x)); {x, t |> bdd_helper} end) |> Map.new} end) |> Map.new
   end
 
   def build_dfa_delta(powerset, alphabet, nfa_delta) do
@@ -205,7 +221,6 @@ defmodule Automata do
     f_prime = q_prime |> prime_accept(f)
 
     {q_prime, al_prime, d_prime, q0_prime, f_prime} |> prune
-    # {q_prime, al_prime, d_prime, q0_prime, f_prime}
   end
 
   def e_determinize_noprune({q, al, d, q0, f}) do
@@ -213,6 +228,17 @@ defmodule Automata do
     al_prime = MapSet.delete(al, :epsilon)
     d_prime = q_prime |> e_build_dfa_delta(al_prime, d)
     q0_prime = MapSet.new([e_closure(d, q0)])
+    f_prime = q_prime |> prime_accept(f)
+
+    {q_prime, al_prime, d_prime, q0_prime, f_prime}
+  end
+
+  def e_determinize2({_q, al, d, q0, f}) do
+
+    al_prime = MapSet.delete(al, :epsilon)
+    q0_prime = MapSet.new([e_closure(d, q0)])
+    d_prime = e_build_dfa_delta2(d, al_prime, %{}, q0_prime |> MapSet.to_list, MapSet.new())
+    q_prime = d_prime |> Map.keys() |> MapSet.new
     f_prime = q_prime |> prime_accept(f)
 
     {q_prime, al_prime, d_prime, q0_prime, f_prime}
@@ -243,7 +269,6 @@ defmodule Automata do
   def prune({q, al, d, q0, f}) do
     prune_helper({q, al, d, q0, f}, d |> drop_alphabet)
   end
-
 
 end
 
